1754eac7da03f4b80793ac710c768083dfe5986c
Brian Chao <79382361+bbchao@users.noreply.github.com>
2023-01-29 13:49:06 -0800

    Update A1.R
    made some small changes to the nightHoursAvgDf dataframe - hours now start at 18:00 and end at 6:00.

--- A1.R
+++ A1.R
@@ -3,7 +3,8 @@
 library(ggplot2)  
 library('psych') 
 library(corrplot)
- 
+library(dplyr)
+library(zoo)
 DataDf <- read.table("Group_Assignment_1_Dataset.txt", header = TRUE, sep = ",")
 DataDf$Date <- as.POSIXlt(DataDf$Date, format = "%d/%m/%Y")
 
@@ -15,7 +16,20 @@ week2$TimeModified <- as.POSIXlt(week2$Time, format = "%H:%M:%S")
 week2 <- subset(week2, select = -c(Time))
 Time <- subset(week2$TimeModified, week2$Date >= as.POSIXlt("8/1/2007",format = "%d/%m/%Y"))
 week2 <- cbind(week2, Time)
-
+week2 <- week2 %>%
+  mutate(Global_active_power = na.approx(Global_active_power))
+week2 <- week2 %>%
+  mutate(Global_reactive_power = na.approx(Global_reactive_power))
+week2 <- week2 %>%
+  mutate(Voltage = na.approx(Voltage))
+week2 <- week2 %>%
+  mutate(Global_intensity = na.approx(Global_intensity))
+week2 <- week2 %>%
+  mutate(Sub_metering_1 = na.approx(Sub_metering_1))
+week2 <- week2 %>%
+  mutate(Sub_metering_2 = na.approx(Sub_metering_2))
+week2 <- week2 %>%
+  mutate(Sub_metering_3 = na.approx(Sub_metering_3))
 
 ## 1
 
@@ -24,6 +38,10 @@ getmode <- function(v) {
   uniqv[which.max(tabulate(match(v, uniqv)))]
 } # https://www.tutorialspoint.com/r/r_mean_median_mode.htm
 
+
+
+
+
 wday <- c(as.POSIXlt(week2$Date, format = "%d/%m/%Y")$wday)
 week2 <- cbind(week2, wday)
 weekdaysDayHoursDf <- with(week2, week2[(wday != 0 & wday != 6) & TimeModified >= as.POSIXlt("06:00:00", format = "%H:%M:%S") & TimeModified < as.POSIXlt("18:00:00", format = "%H:%M:%S"), ])
@@ -31,10 +49,14 @@ weekdaysNightHoursDf <- with(week2, week2[(wday != 0 & wday != 6) & (TimeModifie
 weekendDayHoursDf <- with(week2, week2[(wday == 0 | wday == 6) & TimeModified >= as.POSIXlt("06:00:00", format = "%H:%M:%S") & TimeModified < as.POSIXlt("18:00:00", format = "%H:%M:%S"), ])
 weekendNightHoursDf <- with(week2, week2[(wday == 0 | wday == 6) & (TimeModified < as.POSIXlt("06:00:00", format = "%H:%M:%S") | TimeModified >= as.POSIXlt("18:00:00", format = "%H:%M:%S")), ])
 
+
+
+
+
 # Global_active_power (A)
 globalActivePowerArithMean <- mean(week2$Global_active_power)
 globalActivePowerGeoMean <- geometric.mean(week2$Global_active_power)
-globalActivePowerMedian <- median(week2$Global_active_power, na.rm = TRUE)
+globalActivePowerMedian <- median(week2$Global_active_power)
 globalActivePowerMode <- getmode(week2$Global_active_power)
 globalActivePowerStdDev <- sd(week2$Global_active_power)
 
@@ -49,11 +71,11 @@ globalActivePowerWeekendNightMax <- max(weekendNightHoursDf$Global_active_power)
 globalActivePowerWeekendNightMin <- min(weekendNightHoursDf$Global_active_power)
 
 # Global_reactive_power (B)
-globalReactivePowerArithMean <- mean(week2$Global_reactive_power, na.rm = TRUE)
+globalReactivePowerArithMean <- mean(week2$Global_reactive_power)
 globalReactivePowerGeoMean <- geometric.mean(week2$Global_reactive_power)
-globalReactivePowerMedian <- median(week2$Global_reactive_power, na.rm = TRUE)
+globalReactivePowerMedian <- median(week2$Global_reactive_power)
 globalReactivePowerMode <- getmode(week2$Global_reactive_power)
-globalReactivePowerStdDev <- sd(week2$Global_reactive_power, na.rm = TRUE)
+globalReactivePowerStdDev <- sd(week2$Global_reactive_power)
 
 globalReactivePowerWeekdayDayMax <- max(weekdaysDayHoursDf$Global_reactive_power)
 globalReactivePowerWeekdayDayMin <- min(weekdaysDayHoursDf$Global_reactive_power)
@@ -62,15 +84,15 @@ globalReactivePowerWeekdayNightMin <- min(weekdaysNightHoursDf$Global_reactive_p
 
 globalReactivePowerWeekendDayMax <- max(weekendDayHoursDf$Global_reactive_power)
 globalReactivePowerWeekendDayMin <- min(weekendDayHoursDf$Global_reactive_power)
-globalReactivePowerWeekendNightMax <- max(weekendNightHoursDf$Global_reactive_power, na.rm = TRUE)
-globalReactivePowerWeekendNightMin <- min(weekendNightHoursDf$Global_reactive_power, na.rm = TRUE)
+globalReactivePowerWeekendNightMax <- max(weekendNightHoursDf$Global_reactive_power)
+globalReactivePowerWeekendNightMin <- min(weekendNightHoursDf$Global_reactive_power)
 
 # Voltage (C)
-VoltageArithMean <- mean(week2$Voltage, na.rm = TRUE)
-VoltageGeoMean <- geometric.mean(week2$Voltage, na.rm = TRUE)
-VoltageMedian <- median(week2$Voltage, na.rm = TRUE)
+VoltageArithMean <- mean(week2$Voltage)
+VoltageGeoMean <- geometric.mean(week2$Voltage)
+VoltageMedian <- median(week2$Voltage)
 VoltageMode <- getmode(week2$Voltage)
-VoltageStdDev <- sd(week2$Voltage, na.rm = TRUE)
+VoltageStdDev <- sd(week2$Voltage)
 
 
 
@@ -121,8 +143,17 @@ matrix2 <- corrplot(corrCoeffs, method="number")
 
 dayHoursAvgDf <- with(week2, week2[(wday == 1) & TimeModified >= as.POSIXlt("06:00:00", format = "%H:%M:%S") & TimeModified < as.POSIXlt("18:00:00", format = "%H:%M:%S"), ])
 dayHoursAvgDf <- subset(dayHoursAvgDf, select=c("TimeModified", "Time"))
-nightHoursAvgDf <- with(week2, week2[(wday == 0) & (TimeModified < as.POSIXlt("06:00:00", format = "%H:%M:%S") | TimeModified >= as.POSIXlt("18:00:00", format = "%H:%M:%S")), ])
-nightHoursAvgDf <- subset(nightHoursAvgDf, select=c("TimeModified", "Time"))
+#nightHoursAvgDf <- with(week2, week2[(wday == 0) & (TimeModified > as.POSIXlt("18:00:00", format = "%H:%M:%S") | TimeModified <= as.POSIXlt("06:00:00", format = "%H:%M:%S")), ])
+nightHoursAvgDf_1 <- with(week2, week2[(wday == 0) & (TimeModified >= as.POSIXlt("18:00:00", format = "%H:%M:%S") & TimeModified <= as.POSIXlt("24:00:00", format = "%H:%M:%S")), ])
+nightHoursAvgDf_2 <- with(week2, week2[(wday == 0) & (TimeModified >= as.POSIXlt("0:00:00", format = "%H:%M:%S") & TimeModified < as.POSIXlt("06:00:00", format = "%H:%M:%S")), ])
+
+nightHoursAvgDf_1 <- subset(nightHoursAvgDf_1, select=c("TimeModified", "Time"))
+nightHoursAvgDf_2 <- subset(nightHoursAvgDf_2, select=c("TimeModified", "Time"))
+print(nightHoursAvgDf_1)
+print(nightHoursAvgDf_2)
+nightHoursAvgDf <- rbind(nightHoursAvgDf_1, nightHoursAvgDf_2)
+print(nightHoursAvgDf)
+
 
 weekdayDayGlobalIntensity <- vector(length=nrow(dayHoursAvgDf))
 weekdayNightGlobalIntensity <- vector(length=nrow(nightHoursAvgDf))
@@ -133,9 +164,9 @@ weekendNightGlobalIntensity <- vector(length=nrow(nightHoursAvgDf))
 # average for weekday day hours
 for(i in 1:nrow(dayHoursAvgDf)) 
 {
- timeDf <- with(weekdaysDayHoursDf, weekdaysDayHoursDf[TimeModified == dayHoursAvgDf[i,1], ])
- average <- mean(timeDf$Global_intensity, na.rm = TRUE)
- weekdayDayGlobalIntensity[i] <- average
+  timeDf <- with(weekdaysDayHoursDf, weekdaysDayHoursDf[TimeModified == dayHoursAvgDf[i,1], ])
+  average <- mean(timeDf$Global_intensity)
+  weekdayDayGlobalIntensity[i] <- average
 }
 dayHoursAvgDf$weekday_average <- weekdayDayGlobalIntensity
 
@@ -143,16 +174,17 @@ dayHoursAvgDf$weekday_average <- weekdayDayGlobalIntensity
 for(i in 1:nrow(nightHoursAvgDf)) 
 {
   timeDf <- with(weekdaysNightHoursDf, weekdaysNightHoursDf[TimeModified == nightHoursAvgDf[i,1], ])
-  average <- mean(timeDf$Global_intensity, na.rm = TRUE)
+  average <- mean(timeDf$Global_intensity)
   weekdayNightGlobalIntensity[i] <- average
 }
 nightHoursAvgDf$weekday_average <- weekdayNightGlobalIntensity
+str(nightHoursAvgDf)
 
 # average for weekend day hours
 for(i in 1:nrow(dayHoursAvgDf)) 
 {
   timeDf <- with(weekendDayHoursDf, weekendDayHoursDf[TimeModified == dayHoursAvgDf[i,1], ])
-  average <- mean(timeDf$Global_intensity, na.rm = TRUE)
+  average <- mean(timeDf$Global_intensity)
   weekendDayGlobalIntensity[i] <- average
 }
 dayHoursAvgDf$weekend_average <- weekendDayGlobalIntensity
@@ -161,7 +193,7 @@ dayHoursAvgDf$weekend_average <- weekendDayGlobalIntensity
 for(i in 1:nrow(nightHoursAvgDf)) 
 {
   timeDf <- with(weekendNightHoursDf, weekendNightHoursDf[TimeModified == nightHoursAvgDf[i,1], ])
-  average <- mean(timeDf$Global_intensity, na.rm = TRUE)
+  average <- mean(timeDf$Global_intensity)
   weekendNightGlobalIntensity[i] <- average
 }
 nightHoursAvgDf$weekend_average <- weekendNightGlobalIntensity
@@ -169,6 +201,7 @@ nightHoursAvgDf$weekend_average <- weekendNightGlobalIntensity
 dayHoursAvgDf <- subset(dayHoursAvgDf, select = -c(TimeModified))
 nightHoursAvgDf <- subset(nightHoursAvgDf, select = -c(TimeModified))
 
+print(nightHoursAvgDf)
 
 # Linear Regression
 
@@ -184,16 +217,60 @@ fit <- lm(formula=weekday_average ~ Time, data=nightHoursAvgDf)
 weekday_predicted <- predict(fit, nightHoursAvgDf)
 nightHoursAvgDf = cbind(nightHoursAvgDf, weekday_predicted)
 
+print(nightHoursAvgDf)
+
 fit <- lm(formula=weekend_average ~ Time, data=nightHoursAvgDf)
 weekend_predicted <- predict(fit, nightHoursAvgDf)
 nightHoursAvgDf = cbind(nightHoursAvgDf, weekend_predicted)
 
+print(nightHoursAvgDf)
+
+nt <- 0:719
+nightHoursAvgDf$newTime<-nt
+
 
 # Plot Linear Regression
-
+#daytime
 ggplot(dayHoursAvgDf, aes(x=Time)) + 
   geom_line(aes(y = weekday_predicted), color = "darkred") + 
   geom_point(aes(y = weekday_average), color="darkred") +
   geom_line(aes(y = weekend_predicted), color="steelblue", linetype="twodash") +
   geom_point(aes(y = weekend_average), color="steelblue")
+
+
+#nighttime
+ggplot(nightHoursAvgDf, aes(x=newTime)) + 
+  geom_line(aes(y = weekday_predicted), color = "darkred") + 
+  geom_point(aes(y = weekday_average), color="darkred") +
+  geom_line(aes(y = weekend_predicted), color="steelblue", linetype="twodash") +
+  geom_point(aes(y = weekend_average), color="steelblue")
+
+
+
+
+
+
+weekdayDay <- with(dayHoursAvgDf, aggregate(list(weekday_average = dayHoursAvgDf$weekday_average), list(Time=tolower(Time)), mean))
+weekdayNight <- with(nightHoursAvgDf, aggregate(list(weekday_average = nightHoursAvgDf$weekday_average), list(Time=tolower(Time)), mean))
+weekendDay <- with(dayHoursAvgDf, aggregate(list(weekend_average = dayHoursAvgDf$weekend_average), list(Time=tolower(Time)), mean))
+weekendNight <- with(nightHoursAvgDf, aggregate(list(weekend_average = nightHoursAvgDf$weekend_average), list(Time=tolower(Time)), mean))
+
+
+
+
+weekdayDay$newTime<-nt
+weekdayNight$newTime<-nt
+weekendDay$newTime<-nt
+weekendNight$newTime<-nt
+
+
+
+ggplot()+
+  geom_point(data=weekdayDay, mapping=aes(x=newTime, y=weekday_average), color="red") +
+  geom_point(data=weekdayNight, mapping=aes(x=newTime, y=weekday_average), color="blue") +
+  geom_point(data=weekendDay, mapping=aes(x=newTime, y=weekend_average), color="green")+
+  geom_point(data=weekendNight, mapping=aes(x=newTime, y=weekend_average), color="orange")
+
+
+
 
