68c7c1840f703d5ac86542f3fd1b01c9c3dd6c56
Mila Kobayashi <77714757+mila-k@users.noreply.github.com>
2023-02-07 13:39:19 -0800

    Add files via upload

---
+++ A2.R
@@ -0,0 +1,82 @@
+## Assignment 2 Part 2
+
+library(TTR)
+library(moments)
+library(dplyr)
+library(ggplot2)  
+library(zoo)
+
+DataDf <- read.table("Group_Assignment_2_Dataset.txt", header = TRUE, sep = ",")
+DataDf$week_num <- strftime(as.Date(DataDf$Date, format = "%d/%m/%Y"), format = "%V")
+DataDf <- subset(DataDf, DataDf$Date != "31/12/2007") # drop 31/12/2007 (Monday) to get complete 52 weeks 
+
+# linear interpolation on NA values
+DataDf$Global_active_power <- na.approx(DataDf$Global_active_power)
+DataDf$Global_reactive_power <- na.approx(DataDf$Global_reactive_power)
+DataDf$Voltage <- na.approx(DataDf$Voltage)
+DataDf$Global_intensity <- na.approx(DataDf$Global_intensity)
+DataDf$Sub_metering_1 <- na.approx(DataDf$Sub_metering_1)
+DataDf$Sub_metering_2 <- na.approx(DataDf$Sub_metering_2)
+DataDf$Sub_metering_3 <- na.approx(DataDf$Sub_metering_3)
+
+
+# Moving Averages
+smoothenedWeeksDf <- data.frame(matrix(nrow = 10080, ncol = 0))
+for (i in 1:52) 
+{
+  week <- subset(DataDf, DataDf$week_num == i | DataDf$week_num == paste0("0", i))
+  moving_average <- SMA(week$Global_intensity, 9)
+  smoothenedWeeksDf <- cbind(smoothenedWeeksDf, moving_average)
+  colnames(smoothenedWeeksDf)[i] <- paste0("smoothened_week_", i)
+}
+
+# average smoothened week
+smoothenedWeeksDf$average_smoothened_week <- rowMeans(smoothenedWeeksDf, na.rm=T)
+smoothenedWeeksDf <- tail(smoothenedWeeksDf, -8) # remove the first 8 NA values produced from sma
+
+
+# Euclidean distance (to measure similarity between the smoothened week and average)
+euclidean <- function(a, b) sqrt(sum((a - b)^2))
+
+
+# find euclidean distance for every week
+euclideanDistances <- vector(length=52)
+for (i in 1:52) 
+{
+  col <- smoothenedWeeksDf %>% select(paste0("smoothened_week_", i))
+  euclideanDistances[i] <- euclidean(col, smoothenedWeeksDf$average_smoothened_week)
+}
+
+
+# Most anomalous week (Max euclidean distance)
+which.max(euclideanDistances)   # week 52
+
+# Least anomalous week (Min euclidean distance)
+which.min(euclideanDistances)   # week 30
+
+# store scores in a dataframe
+scoresDf <- data.frame(week = 1:52, score = euclideanDistances)
+
+# add datetime column for the x axis
+smoothenedWeeksDf$Datetime <- with(DataDf, as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))[9:10080]
+
+
+# plot the smoothened versions of the most and the least anomalous weeks against the average smoothened week
+
+ggplot(smoothenedWeeksDf) +
+  geom_point(aes(x = Datetime, y= smoothened_week_30, colour="Least Anomalous"), size=0.5) +
+  geom_line(aes(x = Datetime, y= average_smoothened_week, colour="Average"), linewidth=1.0) +
+  scale_colour_manual("", values = c("Least Anomalous"="darkred", 
+                                     "Average"="yellow")) +
+  labs(x="Time", y="Global Intensity") +
+  theme(legend.position = "bottom", axis.text.x = element_blank(), plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Least Anomalous Week")
+
+ggplot(smoothenedWeeksDf) +
+  geom_point(aes(x = Datetime, y= smoothened_week_52, colour="Most Anomalous"), size=0.5) +
+  geom_line(aes(x = Datetime, y= average_smoothened_week, colour="Average"), linewidth=1.0) +
+  scale_colour_manual("", values = c("Most Anomalous"="steelblue",
+                                     "Average"="yellow")) +
+  labs(x="Time", y="Global Intensity") +
+  theme(legend.position = "bottom", axis.text.x = element_blank(), plot.title = element_text(hjust = 0.5)) + 
+  ggtitle("Most Anomalous Week")
