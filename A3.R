c150dd9fc7cb7d38a206143e15cc2a28550f5444
mila-kobayashi <mila@launchtrip.com>
2023-02-28 18:19:58 -0800

    updated plots

--- A3.R
+++ A3.R
@@ -9,11 +9,7 @@ DataDf <- read.table("Group_Assignment_3_Dataset.txt", header = TRUE, sep = ",")
 # linear interpolation on NA values
 DataDf$Global_active_power <- na.approx(DataDf$Global_active_power)
 DataDf$Global_reactive_power <- na.approx(DataDf$Global_reactive_power)
-DataDf$Voltage <- na.approx(DataDf$Voltage)
 DataDf$Global_intensity <- na.approx(DataDf$Global_intensity)
-DataDf$Sub_metering_1 <- na.approx(DataDf$Sub_metering_1)
-DataDf$Sub_metering_2 <- na.approx(DataDf$Sub_metering_2)
-DataDf$Sub_metering_3 <- na.approx(DataDf$Sub_metering_3)
 
 
 
@@ -24,10 +20,7 @@ DataDf$Sub_metering_3 <- na.approx(DataDf$Sub_metering_3)
 DataDf$Global_active_power <- scale(DataDf$Global_active_power, center = TRUE, scale = TRUE)
 DataDf$Global_reactive_power <- scale(DataDf$Global_reactive_power, center = TRUE, scale = TRUE)
 DataDf$Global_intensity <- scale(DataDf$Global_intensity, center = TRUE, scale = TRUE)
-DataDf$Voltage <- scale(DataDf$Voltage, center = TRUE, scale = TRUE)
-DataDf$Sub_metering_1 <- scale(DataDf$Sub_metering_1, center = TRUE, scale = TRUE)
-DataDf$Sub_metering_2 <- scale(DataDf$Sub_metering_2, center = TRUE, scale = TRUE)
-DataDf$Sub_metering_3 <- scale(DataDf$Sub_metering_3, center = TRUE, scale = TRUE)
+
 
 
 # Determine a time window for a specific weekday 
@@ -40,17 +33,26 @@ for (i in 1:52)
   WeeksDf <- cbind(WeeksDf, week$Global_intensity)
   colnames(WeeksDf)[i] <- paste0("week_", i)
 }
-WeeksDf$average <- rowMeans(WeeksDf)
+WeeksDf$Average <- rowMeans(WeeksDf)
 WeeksDf$Datetime <- with(DataDf, as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))[1:10080]
 
+# plot averages
 ggplot(WeeksDf[1440:2880, ]) +
-  geom_line(aes(x = Datetime, y= average, colour="Average"), linewidth=0.5) # Choose Tuesday 12:00 - 14:59
+  geom_line(aes(x = Datetime, y= Average, color="Average"), linewidth=0.5) + 
+  ggtitle("Average Global Intensity") +
+  theme(plot.title = element_text(hjust = 0.5))
 
+# plot individual weeks
+ggplot(WeeksDf[1440:2880, ]) +
+  geom_line(aes(x = Datetime, y= week_30, color="Average"), linewidth=0.5) + # Choose Tuesday 18-21
+  labs(y="Global_Intensity") +
+  ggtitle("Global Intensity for Week 2") +
+  theme(plot.title = element_text(hjust = 0.5))
 
-# Extract the same time window (Tuesday 12:00 - 14:59) for each week
+# Extract the same time window (Tuesday 18-21) for each week
 DataDf$weekday <- c(as.POSIXlt(DataDf$Date, format = "%d/%m/%Y")$wday)
 DataDf$Time <- as.POSIXlt(DataDf$Time, format = "%H:%M:%S")
-trainingDf <- subset(DataDf, DataDf$weekday == 2 & DataDf$Time >= as.POSIXlt("12:00:00", format = "%H:%M:%S") & DataDf$Time < as.POSIXlt("15:00:00", format = "%H:%M:%S"))
+trainingDf <- subset(DataDf, DataDf$weekday == 2 & DataDf$Time >= as.POSIXlt("18:00:00", format = "%H:%M:%S") & DataDf$Time < as.POSIXlt("21:00:00", format = "%H:%M:%S"))
 
 
 
@@ -60,6 +62,7 @@ logLik_global_intensity <- vector(length=14)
 bic_global_intensity <- vector(length=14)
 n_vector <- rep(180, 52)
 
+# HMM for global intensity
 for (i in 3:16)
 {
   model <- depmix(Global_intensity~1, data = trainingDf, nstates = i, ntimes = n_vector)
@@ -71,46 +74,13 @@ for (i in 3:16)
   bic_global_intensity[i-2] <- bic
 }
 
-logLik_active_power <- vector(length=14)
-bic_active_power <- vector(length=14)
-
-for (i in 3:16)
-{
-  model <- depmix(Global_active_power~1, data = trainingDf, nstates = i, ntimes = n_vector)
-  fitModel <- fit(model)
-  # summary <- summary(fitModel)
-  bic <- BIC(fitModel)
-  logLik <- logLik(fitModel)
-  logLik_active_power[i-2] <- logLik
-  bic_active_power[i-2] <- bic
-}
-
-logLik_reactive_power <- vector(length=14)
-bic_reactive_power <- vector(length=14)
-
-for (i in 3:16)
-{
-  model <- depmix(Global_reactive_power~1, data = trainingDf, nstates = i, ntimes = n_vector)
-  fitModel <- fit(model)
-  # summary <- summary(fitModel)
-  bic <- BIC(fitModel)
-  logLik <- logLik(fitModel)
-  logLik_reactive_power[i-2] <- logLik
-  bic_reactive_power[i-2] <- bic
-}
-
 HMM_Global_Intensity <- data.frame(bic = bic_global_intensity, logLik = logLik_global_intensity)
-HMM_Active_Power <- data.frame(bic = bic_active_power, logLik = logLik_active_power)
-HMM_Reactive_Power <- data.frame(bic = bic_reactive_power, logLik = logLik_reactive_power)
 
 ggplot(NULL, aes(x=(3:16))) + 
-  geom_line(data = HMM_Global_Intensity, aes(y = bic), color = "darkred") + 
-  geom_line(data = HMM_Global_Intensity,aes(y = logLik), color="blue")
-
-ggplot(NULL, aes(x=(3:16))) + 
-  geom_line(data = HMM_Active_Power, aes(y = bic), color = "darkred") + 
-  geom_line(data = HMM_Active_Power,aes(y = logLik), color="blue")
-
-ggplot(NULL, aes(x=(3:16))) + 
-  geom_line(data = HMM_Reactive_Power, aes(y = bic), color = "darkred") + 
-  geom_line(data = HMM_Reactive_Power,aes(y = logLik), color="blue")
+  geom_line(data = HMM_Global_Intensity, aes(y = bic, colour = "BIC")) + 
+  geom_line(data = HMM_Global_Intensity,aes(y = logLik, colour="LogLik")) +
+  labs(x="nstates", y="values") +
+  ggtitle("LogLik and BIC comparison for n states of HMM (Global Intensity)") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_colour_manual("", values = c("BIC"="darkred", 
+                                     "LogLik"="steelblue")) 
